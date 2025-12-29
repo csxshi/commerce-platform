36ae8b78f01c97e71ec28deec2af91863ec4e623
chengshunxiang <chengshunxiang@gmail.com>
2025-12-29 16:12:52 +0900

    feat:Springboot4初回追加

---
+++ ec-backend/src/test/java/com/ecsite/ecbackend/EcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecsite.ecbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

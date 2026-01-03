fa907c0935cd2fae8c3b3507d912999d60d37e48
chengshunxiang <chengshunxiang@gmail.com>
2026-01-03 14:11:19 +0900

    feat(config):プロジェクトの設定変更  --war->jarに変更

--- ec-backend/src/main/java/com/ecsite/ecbackend/EcBackendApplication.java
+++ ec-backend/src/main/java/com/ecsite/ecbackend/EcBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EcBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EcBackendApplication.class, args);
+	}
 
 }
